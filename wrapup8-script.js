aad0659f937bae785a317372bd406dabdd6b74fd
koalee-lennard <75128039+koalee-lennard@users.noreply.github.com>
2021-01-06 01:10:52 +0800

    Add files via upload

--- wrapup8-script.js
+++ wrapup8-script.js
@@ -1,62 +1,30 @@
-const SecretCode = document.getElementById('secret-code');
-var j;
-var k;
-var l;
-var m;
-var n;
-var o;
-var p;
-var q;
-function submit001() {
-    b = input1.value;
-    c = input2.value;
-    d = input3.value;
-    e = input4.value;
-    f = input5.value;
-    g = input6.value;
-    h = input7.value;
-    i = input8.value;
-    if (b == "phone charger and battery bank"){
-        j = 1;
-        input1.value = b;
-    }
+function validateAnswer(){
+    //var test = document.getElementById("test");
+    //test.innerHTML += "clicked! ";
+    //document.getElementById("test").innerHTML = "checkbox clicked!";
 
-    if (c == "flashlight"){
-        k = 1;
-        input2.value = c;
-    }
+    var a = document.getElementById("input1").value;
+    var b = document.getElementById("input2").value;
+    var c = document.getElementById("input3").value;
+    var d = document.getElementById("input4").value;
+    var e = document.getElementById("input5").value;
+    var f = document.getElementById("input6").value;
+    var g = document.getElementById("input7").value;
+    var h = document.getElementById("input8").value;
 
-    if (d == "radio"){
-        l = 1;
-        input3.value = d;
-    }
-
-    if (e == "first aid kit"){
-        m = 1;
-        input4.value = e;
-    }
-
-    if (f == "seasonal clothing"){
-        n = 1;
-        input5.value = f;
-    }
-
-    if (g == "food and water"){
-        o = 1;
-        input6.value = g;
-    }
-
-    if (h == "emergency plan"){
-        p = 1;
-        input7.value = h;
-    }
-
-    if (i =i= "whistle"){
-        q = 1;
-        input8.value = i;
-    }
-
-    if (j==1 && k==1 && l==1 && m==1 && n==1 && o==1 && p==1 && q==1){
-        SecretCode.classList.remove("hide");
+    if ((a == "PHONE CHARGER AND BATTERY BANK") && 
+    (b == "FLASHLIGHT") && 
+    (c == "RADIO") && 
+    (d == "FIRST AID KIT") &&
+    (e == "SEASONAL CLOTHING") &&
+    (f == "FOOD AND WATER") &&
+    (g == "EMERGENCY PLAN") &&
+    (h == "WHISTLE")
+    ){
+        //document.getElementById("output").innerHTML = "CORRECT!"
+        alert("PASS");
+    } else{
+        //document.getElementById("output").innerHTML ="WRONG!"
+        alert("FAIL");
     }
 }
